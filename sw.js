edb79514834262799520b54de40fb0ae7bed8c35
alinares73 <alvaro.linares.rodriguez@gmail.com>
2026-09-05 13:58:13 +0200

    nv8

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // CADA VEZ QUE CAMBIES EL INDEX.HTML EN EL FUTURO, CAMBIA ESTE NÚMERO (ej. v2, v3, v4...)
-const CACHE_NAME = 'ciudadela-v7';
+const CACHE_NAME = 'ciudadela-v8';
 
 // 1. Instalación de la aplicación en el móvil
 self.addEventListener('install', (e) => {
